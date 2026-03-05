d01db3ddcb581a6207de37fa65fc676e22146799
MouslyCode <rezasubkhan50@gmail.com>
2026-03-05 13:03:07 +0700

    Configured MongoDB

---
+++ app/backend/database/connection.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+)
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Load .env !")
+	}
+	uri := os.Getenv("MONGO_URI")
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("MongoDB Connect Succesfully!")
+}
--- app/backend/main.go
+++ app/backend/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/MouslyCode/three-of-a-perfect-pair/backend/database"
+	"github.com/joho/godotenv"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error Load .env !")
+	}
+
+	database.Connect()
 }
